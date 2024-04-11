22358b1f392c229785fa506743adaca878ff1cd3
daskrys <caperezl@ucsc.edu>
2024-04-10 21:20:40 -0700

    add basic map

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,43 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { Loader } from '@googlemaps/js-api-loader';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+`;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const apiKey:string = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+
+let map;
+async function initMap(): Promise<void> {
+  const position = { lat: 36.9905, lng: -122.0584 };
+
+  // Request needed libraries.
+  const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
+
+  // The map, centered at Uluru
+  map = new Map(
+    document.getElementById('app') as HTMLElement,
+    {
+      zoom: 15,
+      center: position,
+      mapId: 'DEMO_MAP_ID',
+    }
+  );
+
+  // The marker, positioned at UCSC
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: 'University of California, Santa Cruz',
+  });
+}
+
+
+const loader = new Loader({
+  apiKey: apiKey, 
+  version: "weekly",
+});
+
+loader.load().then(() => {
+  initMap();
+}).catch(e => console.error(e));
